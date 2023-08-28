8554f89cc75954105d01a8f53d350a27f418f65f
Abolghasem Taziki <113670236+AbolghasemTz@users.noreply.github.com>
2023-08-28 12:13:56 +0330

    static data and complete UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import { allCharacters } from "../data/data";
 import "./App.css";
+import CharacterDetails from "./components/CharacterDetails";
+import CharacterList from "./components/CharactarList";
 import Navbar from "./components/Navbar";
-
-function App(){
-  return <div className="app">
-    <Navbar />
-  </div>
+function App() {
+  return (
+    <div className="app">
+     
+      <Navbar />
+      <div className="main">
+        <CharacterList characters={allCharacters} />
+        <CharacterDetails />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
